8057883f3311a0b2b04a717eddec6f77b17559cd
Nandang <77793855+BegalNenen@users.noreply.github.com>
2022-10-11 06:50:57 +0700

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect,url_for,session,send_file
 from io import BytesIO
 from pytube import YouTube
-app = Flask(__name__,template_folder='website')
+app = Flask(__name__,template_folder='template')
 app.config['SECRET_KEY'] = "abcdefghijklmnopqrstuvwxyz"
 @app.route('/',methods=["GET","POST"])
 def home():
