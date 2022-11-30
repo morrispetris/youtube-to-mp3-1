836af50cf9983756b31a10f6f49aaa2f79f285d1
morrispetris <112695708+morrispetris@users.noreply.github.com>
2022-11-30 17:20:28 +0800

    Update index.py

--- index.py
+++ index.py
@@ -42,7 +42,7 @@ def startDownload():
         d = pathlib.Path.cwd()
         audio = YouTube(session["link"]).streams.filter(only_audio=True).first()   #for mp3        
         audio.download(output_path=d, filename=fn+".mp3")
-        path = d / fn+".mp3"
+        path = d + "/" + fn +".mp3"
         file_handle = open(path, 'rb')
         return send_file(file_handle, as_attachment=True, download_name=fn+".mp3", mimetype="audio/mp3")
         
