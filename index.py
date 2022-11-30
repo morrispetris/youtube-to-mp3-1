ff8084863ef9ef98254955c694c97d752f6b9a3c
morrispetris <112695708+morrispetris@users.noreply.github.com>
2022-11-30 16:07:26 +0800

    Update index.py

--- index.py
+++ index.py
@@ -28,14 +28,15 @@ def startDownload():
     if request.method == "POST":
         buffer = BytesIO()
         #audio = YouTube(session["link"]).streams.filter(only_audio=True)[4]       #for webm
-        audio = YouTube(session["link"]).streams.filter(only_audio=True).first()   #for mp3
-        #audio = YouTube(session["link"]).streams.get_by_itag(251)                 #for webm
-        audio.stream_to_buffer(buffer)
-        buffer.seek(0)
+        #audio.stream_to_buffer(buffer)
+        #buffer.seek(0)
         #return send_file(buffer,as_attachment=True,download_name=session["title"]+"mp3",mimetype="audio/mpeg")
         #return send_file(buffer,as_attachment=True,download_name=session["title"],mimetype="audio/mp3")
-        return send_file(buffer,as_attachment=True,download_name=session["title"]+".mp3",mimetype="audio/mp3")
-
+        #return send_file(buffer,as_attachment=True,download_name=session["title"]+".mp3",mimetype="audio/mp3")
+        
+        audio = YouTube(session["link"]).streams.filter(only_audio=True).first()   #for mp3
+        audio.download(session["title"]+".mp3")
+        
     return redirect(url_for('home'))
 if __name__ == "__main__":
     app.run(debug=True)
